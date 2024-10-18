9c6582c4c5b7461d5669341b041e1daf41e47466
patrickadr6 <94654253+patrickadr6@users.noreply.github.com>
2024-10-18 19:46:46 -0300

    Update calendario.py

--- calendario.py
+++ calendario.py
@@ -1,6 +1,6 @@
 import datetime, calendar
 
-def separar_dias_do_mes(mes, ano):
+def separar_dias_hemo(mes, ano):
     # Pegar todos os dias do mês
 
     # ! PERGUNTAR MES E ANO NO COMEÇO DO PROGRAMA
@@ -27,3 +27,23 @@ def separar_dias_do_mes(mes, ano):
             terca_quinta_sabado[dia.strftime('%d/%m/%Y')] = 'sab'
 
     return [segunda_quarta_sexta, terca_quinta_sabado]
+
+def separar_segunda_a_sexta(mes, ano):
+    dias_por_mes = calendar.monthrange(ano, mes)[1]
+    lista_dias = [datetime.date(ano, mes, dia) for dia in range(1, dias_por_mes + 1)]
+
+    segunda_a_sexta = {}
+
+    for dia in lista_dias:
+        if dia.weekday() == 0:
+            segunda_a_sexta[dia.strftime('%d/%m/%Y')] = 'seg'
+        elif dia.weekday() == 1:
+            segunda_a_sexta[dia.strftime('%d/%m/%Y')] = 'ter'
+        elif dia.weekday() == 2:
+            segunda_a_sexta[dia.strftime('%d/%m/%Y')] = 'qua'
+        elif dia.weekday() == 3:
+            segunda_a_sexta[dia.strftime('%d/%m/%Y')] = 'qui'
+        elif dia.weekday() == 4:
+            segunda_a_sexta[dia.strftime('%d/%m/%Y')] = 'sex'
+
+    return segunda_a_sexta
