307ee3556196c6a6c54292ccb02e288ccb84c775
patrickadr6 <94654253+patrickadr6@users.noreply.github.com>
2024-10-18 20:30:03 -0300

    Update horario.py

--- horario.py
+++ horario.py
@@ -3,6 +3,6 @@ from datetime import datetime
 def horario_atual():
     current_time = datetime.now()
 
-    horario = f"({current_time.hour}:{current_time.minute})"
+    horario = f"({current_time.day}/{current_time.month}/{current_time.year} {current_time.hour}:{current_time.minute})"
 
     return horario
